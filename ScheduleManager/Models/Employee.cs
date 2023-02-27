7dcc427466bc53bb0f3333471ff1c079f84a5a9e
JamesKelly23 <118563044+JamesKelly23@users.noreply.github.com>
2023-02-27 01:39:05 -0500

    Configured SQL Connection and started with data classes.

---
+++ ScheduleManager/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ScheduleManager.Models
+{
+    public class Employee
+    {
+        public int ID { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }  
+        public int RankID { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public String Phone { get; set; }
+        public DateOnly DOB { get; set; }
+        public Employee(string firstName, string lastName, int rankID, string password, string username, string email, string phone, DateOnly dOB)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            RankID = rankID;
+            Password = password;
+            Username = username;
+            Email = email;
+            Phone = phone;
+            DOB = dOB;
+        }
+        public Employee(int theID)
+        {
+            ID = theID;
+
+        }
+    }
+}
---
+++ ScheduleManager/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿global using Microsoft.Data.SqlClient;
+
+namespace ScheduleManager.Models
+{
+    public class Rank
+    {
+        public int ID { get; }
+        public string Title { get; set; }
+
+        public Rank(int theID)
+        {
+            ID = theID;
+            SqlConnection theConnection = new SqlConnection("Data Source=localhost\\SQLEXPRESS; Integrated Security=true; Initial Catalog='ScheduleManager'");
+            theConnection.Open();
+            SqlCommand theCommand = new SqlCommand("SELECT * FROM Rank WHERE ID=" + ID, theConnection);
+            SqlDataReader theReader = theCommand.ExecuteReader();
+            theReader.Read();
+            Title = theReader.GetString(1);
+            theConnection.Close();
+        }
+    }
+}
