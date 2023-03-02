2098bbcefef934008eb1d6e855a9a4140b1b42d9
JamesKelly23 <118563044+JamesKelly23@users.noreply.github.com>
2023-03-01 21:52:31 -0500

    Added scaffolding for Time Off Request screen.

---
+++ ScheduleManager/Controllers/RequestTimeOff.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScheduleManager.Controllers
+{
+    public class RequestTimeOff : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
