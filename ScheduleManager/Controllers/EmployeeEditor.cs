e5a5ba12ad116f3390b1b784ba7635219f411a5b
MaxwellConnor-Purdue <maxwellconnor@student.purdueglobal.edu>
2023-03-03 18:22:58 -0500

    Can now search for Employee Details (Will throw error if no result found)

--- ScheduleManager/Controllers/EmployeeEditor.cs
+++ ScheduleManager/Controllers/EmployeeEditor.cs
@@ -1,26 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using ScheduleManager.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 namespace ScheduleManager.Controllers
 {
     public class EmployeeEditor : Controller
     {
+  
         public IActionResult EditorIndex()
         {
-            EmployeeDetails(6);
-            return View("EmployeeDetails");
+
+            return View();
 
         }
         public IActionResult Edit()
         {
             return View();
         }
+        //[HttpPost]
         public IActionResult EmployeeDetails(int a)
         {
             
             ViewBag.currentDetails = new Models.Employee(a);
-            return View();
+            return View("EmployeeDetails");
+        }
+        public IActionResult Search(int EmplyID)
+        {
+            try
+            {
+                //b = Convert.ToInt32((HttpContext.Request.Form["EmplyID"]));
+
+                EmployeeDetails(EmplyID);
+                return View("EmployeeDetails");
+            }
+            catch (Exception ex) 
+            {
+                if (ViewBag.currentDetails == false)
+                {
+                    return View("EditorIndex");
+                }
+                else
+                {
+                    throw ex;
+                }
+                
+            }
+
         }
         public IActionResult Update() 
         {
