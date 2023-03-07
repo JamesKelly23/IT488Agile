7160fce8efd287c1797f0cdaacc8b5dfe8af2cfe
wwhite14 <williamwhite@student.purdueglobal.edu>
2023-03-06 17:12:14 -0800

    Changes to RequestTimeOff

--- ScheduleManager/Controllers/RequestTimeOff.cs
+++ ScheduleManager/Controllers/RequestTimeOff.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScheduleManager.Models;
+
 
 namespace ScheduleManager.Controllers
 {
     public class RequestTimeOff : Controller
     {
+        //GET:RequestTimeOff
         public IActionResult Index()
         {
-            return View();
+            //Display the form for RequestTimeOff 
+            //Code to retrieve data from database
+            //Pass it to index
+            int loggedInEmployee = HttpContext.Session.GetInt32("_LoggedInEmployeeID") ?? 0;
+            if (loggedInEmployee == 0)
+            {
+
+            }
+            else
+            {
+                ViewData["LoggedIn"] = 1;
+                ViewBag.CurrentUser = new Employee(loggedInEmployee);
+            }
+            return View("Index");
+
+
         }
+        //POST: RequestTimeoff request
+
+        public IActionResult Submit()
+        {
+
+            TimeOffRequest TheRequest = new(employeeID, HttpContext.Session.GetInt32("_LoggedInEmployeeID") ?? 0)
+            return View("Index");
+
+        } 
     }
 }
