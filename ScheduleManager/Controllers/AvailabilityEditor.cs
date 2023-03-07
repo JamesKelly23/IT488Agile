ad63efadf9ebff99ffcae3c7f5a505dfa8fcd068
Kent Shrock <kentshrock@student.purdueglobal.edu>
2023-03-07 18:58:45 -0500

    Fixed problem with form

--- ScheduleManager/Controllers/AvailabilityEditor.cs
+++ ScheduleManager/Controllers/AvailabilityEditor.cs
@@ -20,7 +20,7 @@ namespace ScheduleManager.Controllers
         }
 
 
-        public IActionResult Undo()
+        public IActionResult Reset()
         {
             //int theID = 0;
             //int theID = ViewBag.CurrentUser.ID;
@@ -44,9 +44,42 @@ namespace ScheduleManager.Controllers
             // HttpContext.Session.SetInt32("_LoggedInRank", new Employee(theID).RankID);
 
 
+
+           
+
+
             ViewBag.CurrentUser = new Employee(loggedInEmployee);
             ViewBag.CurrentAvailability = new Availability(loggedInEmployee);
+
+            // ViewBag.CurrentAvailability.MondayEnd = "1/1/2001 14:00:00";
+             DateTime theTime = Convert.ToDateTime("1/1/2001 19:00:00");
+            theTime = Convert.ToDateTime(HttpContext.Request.Form["m_end"]);
+
+
+            ViewBag.CurrentAvailability.MondayEnd = theTime;
+          //  Availability theAvail = new(loggedInEmployee);
+            //ViewBag.CurrentAvailability.MondayEnd = Convert.ToDateTime(HttpContext.Request.Form["m_start"]);
+          //  theAvail.MondayEnd = Convert.ToDateTime(HttpContext.Request.Form["m_start"]);
+
+            //theAvail.Save();
+            ViewBag.CurrentAvailability.Save();
+
+
+            //  ViewBag.CurrentAvailability =theAvail;
+
             ViewData["Message"] = "Update Complete!";
+
+            /*
+
+            //The problem is that I can't read from the view
+            ViewData["Message"] = Convert.ToDateTime(HttpContext.Request.Form["m_end"]); 
+            if (Convert.ToDateTime(HttpContext.Request.Form["m_end"])== Convert.ToDateTime(HttpContext.Request.Form["m_start"]))
+            {
+                ViewData["Message"] = "Same";
+            }
+           // ViewData["Message"] = Convert.ToDateTime(HttpContext.Request.Form["m_end"]); 
+            // ViewData["Message"] = theAvail.MondayEnd.ToString();
+            */
             return View("Index");
         }
       
