058e7ab3300e35f4948527d07d877bcb3b545bdc
Maxwell Connor <max.a.connor@outlook.com>
2023-03-08 20:39:51 -0500

    Created Controller and view page for Schedule Editor (should only show for Managers and GMs). Testing GIT and VS on new machine

---
+++ ScheduleManager/Controllers/ScheduleEditor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScheduleManager.Controllers
+{
+    public class ScheduleEditor : Controller
+    {
+        public IActionResult ScheduleEditorIndex()
+        {
+            return View();
+        }
+    }
+}
