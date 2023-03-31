de534e7eb70e110fbe32d4ddd064065dec62833f
JamesKelly23 <118563044+JamesKelly23@users.noreply.github.com>
2023-03-30 21:34:09 -0400

    Added comments to Schedule Composer, Rank LIst, Role List, and Utility Screen.

--- ScheduleManager/Controllers/Composer.cs
+++ ScheduleManager/Controllers/Composer.cs
@@ -6,7 +6,7 @@ namespace ScheduleManager.Controllers
     public class Composer : Controller
     {
         [AuthenticateGM]
-        public IActionResult Index(int? modifier, bool? showavailabilities)
+        public IActionResult Index(int? modifier, bool? showavailabilities) //Show a week at a time and generate links to the painter action for each day
         {
             ViewBag.Modifier = modifier ?? 0;
             ViewBag.ShowAvailabilities = showavailabilities ?? false;
@@ -15,9 +15,8 @@ namespace ScheduleManager.Controllers
             {
                theDate = theDate.AddDays(-1);
             }
-            ViewData["DateRange"] = theDate.ToString("d") + " - " + theDate.AddDays(6).ToString("d");
+            ViewData["DateRange"] = theDate.ToString("d") + " - " + theDate.AddDays(6).ToString("d"); //Supply view with the date range to display at the time of the screen as a string
             ViewBag.StartDate = theDate;
-            int loggedInID = HttpContext.Session.GetInt32("_LoggedInEmployeeID") ?? 0;
             List<Models.Employee> employeeList = Models.Employee.GetList();
             List<Models.Shift> shiftList = Models.Shift.GetScheduleByDate(theDate, theDate.AddDays(6));
             ViewBag.EmployeeList = employeeList;
@@ -25,9 +24,9 @@ namespace ScheduleManager.Controllers
             return View("Index");
         }
         [AuthenticateGM]
-        public IActionResult Painter(int? id, DateTime? date) 
+        public IActionResult Painter(int? id, DateTime? date) //Show a grid. X-Axis:Hours, Y-Axis:Employees and allow the user to paint shifts onto the day using two clicks
         {
-            if(date == null)
+            if(date == null) //URL has most likely been tampered with.
             {
                 ViewData["Message"] = "There was no date supplied to the Painter Action";
                 return View("Error");
@@ -38,7 +37,7 @@ namespace ScheduleManager.Controllers
                 ViewBag.StartTime = Convert.ToDateTime(HttpContext.Request.Form["StartTime"]);
             }
             catch { }
-            if(HttpContext.Request.Method == "POST")
+            if(HttpContext.Request.Method == "POST") //A Shift has been started, pass the known values to the view
             {
                 ViewBag.RoleText = HttpContext.Request.Form["RoleTextBox"];
                 if(ViewBag.RoleText == "")
@@ -53,9 +52,9 @@ namespace ScheduleManager.Controllers
             return View("Painter");
         }
         [AuthenticateGM]
-        public IActionResult AddShift()
+        public IActionResult AddShift() //The End time has been selected, finalize and insert the shift into the database
         {
-            int loggedInID = HttpContext.Session.GetInt32("_LoggedInEmployeeID") ?? 0;
+            //Pull the shift details from the POST Request
             int empID = Convert.ToInt32(HttpContext.Request.Form["EmployeeID"]);
             DateTime theDate = Convert.ToDateTime(HttpContext.Request.Form["TargetDate"]);
             DateTime startTime = Convert.ToDateTime(HttpContext.Request.Form["StartTime"]);
@@ -66,16 +65,18 @@ namespace ScheduleManager.Controllers
             {
                 Role = HttpContext.Request.Form["RoleTextBox"];
             }
+            //Use the POST values to instantiate a Shift object
             Shift theShift = new Shift(false, empID, theDate, startTime, endTime, Role, Notes);
-            theShift.Save();
+            theShift.Save(); //Insert record into database
             return Painter(null, theDate);
         }
         [AuthenticateGM]
-        public IActionResult DeleteShift(int id)
+        public IActionResult DeleteShift(int id) //Delete button has been clicked, delete the requested shift.
         {
+            //Use GET data to instantiate the shift object
             Shift theShift = new Shift(id);
             DateTime theDate = theShift.ShiftDate;
-            theShift.Delete();
+            theShift.Delete(); //Delete the record from the database.
             return Painter(null, theDate);
         }
     }
--- ScheduleManager/Controllers/Utility.cs
+++ ScheduleManager/Controllers/Utility.cs
@@ -6,18 +6,18 @@ namespace ScheduleManager.Controllers
     public class Utility : Controller
     {
         [AuthenticateManager]
-        public IActionResult Index()
+        public IActionResult Index() //Generate the view unfiltered. No data is required
         {
             return View();
         }
         [AuthenticateManager]
-        public IActionResult RoleManager()
+        public IActionResult RoleManager() //Show the Role Manager View and pass it the list of roles from the database
         {
             ViewBag.RoleList = Models.Role.GetList();
             return View("RoleManager");
         }
         [AuthenticateManager]
-        public IActionResult UpdateRole(int id)
+        public IActionResult UpdateRole(int id) //Update the selected role with the content from the form
         {
             Role theRole = new(id);
             theRole.Name = HttpContext.Request.Form["NewRoleName"];
@@ -25,25 +25,25 @@ namespace ScheduleManager.Controllers
             return RoleManager();
         }
         [AuthenticateManager]
-        public IActionResult EditRole(int id)
+        public IActionResult EditRole(int id) //Show the RoleManager view but pass it the ID of the role that needs an edit form rendered
         {
             ViewData["RoleToEdit"] = id;
             return RoleManager();
         }
         [AuthenticateManager]
-        public IActionResult AddRole()
+        public IActionResult AddRole() //Add a new role to the database and generate the RoleManager View
         {
-            new Role(HttpContext.Request.Form["NewRoleName"]).Save();
+            new Role(HttpContext.Request.Form["NewRoleName"]).Save(); //Instantiate a new Role object with the data from the POST and save it to the database, then call the RoleManager Action
             return RoleManager();
         }
         [AuthenticateManager]
-        public IActionResult RankList()
+        public IActionResult RankList() //Show the RankList view and pass it the dlist of Ranks from the database
         {
             ViewBag.RankList=Models.Rank.GetList();
             return View("RankList");
         }
         [AuthenticateManager]
-        public IActionResult DeleteRole(int id)
+        public IActionResult DeleteRole(int id) //Delete a role from the database and then call the RoleManager Action
         {
             Models.Role.Delete(id);
             return RoleManager();
